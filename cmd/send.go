3ba8cd4514ced292618af2b5ed29c1080955b7de
Bidhan Baniya <sandeep.dhakal@midastechnologies.com.np>
2025-12-04 20:32:23 +0545

    cmd part done

---
+++ cmd/send.go
@@ -0,0 +1,38 @@
+package cmd
+
+import (
+	"log"
+
+	"github.com/spf13/cobra"
+)
+
+var sendFilePath string
+var sendTo string
+var sendChunkSize int64
+
+var sendCmd = &cobra.Command{
+	Use:   "send",
+	Short: "Send a single file",
+	Run: func(cmd *cobra.Command, args []string) {
+		if sendFilePath == "" || sendTo == "" {
+			log.Fatal("file and to are required")
+		}
+		cfg := client.UploaderConfig{
+			ServerURL: sendTo,
+			Token:     authToken,
+			ChunkSize: sendChunkSize,
+		}
+		u := client.NewUploader(cfg)
+		err := u.SendFile(sendFilePath, "")
+		if err != nil {
+			log.Fatalf("send error: %v", err)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(sendCmd)
+	sendCmd.Flags().StringVar(&sendFilePath, "file", "", "file to send")
+	sendCmd.Flags().StringVar(&sendTo, "to", "", "server url")
+	sendCmd.Flags().Int64Var(&sendChunkSize, "chunk-size", 4*1024*1024, "chunk size in bytes")
+}
---
+++ cmd/serve.go
@@ -0,0 +1,35 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/spf13/cobra"
+)
+
+var servePort int
+var serveDir string
+
+var serveCmd = &cobra.Command{
+	Use:   "serve",
+	Short: "Start server to receive files",
+	Run: func(cmd *cobra.Command, args []string) {
+		cfg := server.Config{
+			Port:      servePort,
+			BaseDir:   serveDir,
+			AuthToken: authToken,
+		}
+		err := server.Run(cfg)
+		if err != nil {
+			log.Fatalf("server error: %v", err)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(serveCmd)
+	serveCmd.Flags().IntVar(&servePort, "port", 8080, "port to listen on")
+	serveCmd.Flags().StringVar(&serveDir, "dir", "./received", "directory to write files")
+	serveCmd.MarkFlagDirname("dir")
+	fmt.Print("")
+}
